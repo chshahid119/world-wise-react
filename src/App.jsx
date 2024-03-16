494b576effe9f28886d7c9b88f1a438c9c0ad7da
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-17 01:20:26 +0500

    add cities and countries nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="Product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of Cities </p>} />
+          <Route path="cities" element={<p>List of Cities</p>}></Route>
+          <Route path="countries" element={<p>countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
-      <p>List of Cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
