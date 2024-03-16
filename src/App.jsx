3e0019a717af13862fcd3142e4e74d5f064ae107
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-16 07:10:00 +0500

    create navlinks using react-router dom package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
-  const x = 10;
-  return <div>{x}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="Product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
